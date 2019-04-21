1a20cd611b628471c2f61c77475006118b5acc68
Renzo Crisostomo <renzo@crisostomo.me>
2019-04-21 11:48:27 +0200

    Add graphics pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <SDL2/SDL.h>
 #include <glad/glad.h>
 #include <iostream>
+#include <fstream>
 
-using namespace std;
+std::string openShaderSource(std::string filePath);
 
 int main() {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        cout << "Error initializing SDL: " << SDL_GetError() << endl;
+        std::cout << "Error initializing SDL: " << SDL_GetError() << std::endl;
         exit(1);
     }
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
@@ -15,14 +16,54 @@ int main() {
     SDL_Window *window = SDL_CreateWindow("renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, SDL_WINDOW_OPENGL);
     SDL_GLContext glContext = SDL_GL_CreateContext(window);
     if (!gladLoadGLLoader((GLADloadproc) SDL_GL_GetProcAddress)) {
-        cout << "Failed to initialize the OpenGL context." << std::endl;
+        std::cout << "Failed to initialize the OpenGL context." << std::endl;
         exit(1);
     }
-    cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << endl;
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+    std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
+
+    std::string source;
+    const GLchar *src;
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    source = openShaderSource("glsl/vertex.glsl");
+    src = source.c_str();
+    glShaderSource(vertexShader, 1, &src, NULL);
+    glCompileShader(vertexShader);
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    source = openShaderSource("glsl/fragment.glsl");
+    src = source.c_str();
+    glShaderSource(fragmentShader, 1, &src, NULL);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    float vertices[] = {
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f
+    };
+
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
+
     bool quit = false;
     while (!quit) {
         SDL_Event event;
@@ -31,8 +72,23 @@ int main() {
                 quit = true;
             }
         }
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        SDL_GL_SwapWindow(window);
     }
     SDL_Quit();
     return 0;
+}
 
+std::string openShaderSource(std::string filePath) {
+    std::ifstream file(filePath);
+    std::string source;
+
+    file.seekg(0, std::ios::end);
+    source.reserve(file.tellg());
+    file.seekg(0, std::ios::beg);
+
+    source.assign((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    return source;
 }
